ce61b2085934f670c424584796091b5cd610c17b
heather frank <sjcswank@gmail.com>
2024-02-16 14:23:53 -0600

    added sign up page

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<p>Login</p>'
+
+    return render_template("login.html")
 
 
 @auth.route('/logout')
@@ -14,6 +15,24 @@ def logout():
     return '<p>Logout</p>'
 
 
-@auth.route('sign-up')
+@auth.route('sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return '<p>Sign Up</p>'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        if len(firstName) < 2:
+            flash('First name must be greater than 1 character', category='error')
+        if password1 != password2:
+            flash('Passwords do not match', category='error')
+        if len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            flash('Acount created!', category='success')
+
+    return render_template("sign-up.html")
+
