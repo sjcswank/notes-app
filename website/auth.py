a87e08111fadd252c6db48362cd80364160b1f1e
heather frank <sjcswank@gmail.com>
2024-02-17 11:58:11 -0600

    Added login functionality

--- website/auth.py
+++ website/auth.py
@@ -9,7 +9,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
 
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
     return render_template("login.html")
 
 
@@ -26,13 +38,16 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters', category='error')
-        if len(first_name) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character', category='error')
-        if password1 != password2:
+        elif password1 != password2:
             flash('Passwords do not match', category='error')
-        if len(password1) < 7:
+        elif len(password1) < 7:
             flash('Password must be at least 7 characters', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1))
